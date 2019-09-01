bafb2f180c2f12948bc2527692455f8368261579
Bob Creigh <bcreigh@bobcreigh.com>
2019-09-01 17:22:34 -0400

    Updates

---
+++ precache-manifest.23f0a4ecc98a8bcf295f6bdab91d7188.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf1ad9c3a775b6d01162d769e36deb40",
+    "url": "/monstersRolodex/index.html"
+  },
+  {
+    "revision": "c388bdceb60efbd6e1a0",
+    "url": "/monstersRolodex/static/css/main.1fef0494.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monstersRolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "c388bdceb60efbd6e1a0",
+    "url": "/monstersRolodex/static/js/main.5999e203.chunk.js"
+  },
+  {
+    "revision": "efe1ad8b39764438d3d5",
+    "url": "/monstersRolodex/static/js/runtime~main.7cb08802.js"
+  }
+]);
